615b0fa7cc3c8d2cd9b4c72cfcc63a70a7352c1d
ethanwyman <ethan.wyman@maine.edu>
2025-11-22 15:11:19 -0500

    Update int_sort.py
    Format int_sort.py with Black

--- basic_sort_UNIQUE_SUFFIX/int_sort.py
+++ basic_sort_UNIQUE_SUFFIX/int_sort.py
@@ -20,6 +20,7 @@
 This module sorts lists of integers...
 """
 
+
 def bubble(int_list):
     """
     Sorts a list of integers using the bubble sort algorithm.
@@ -35,16 +36,17 @@ def bubble(int_list):
         bubble([3, 4, 5, 2, 1, 1, 5])
         bubble sort[1, 1, 2, 3, 4, 5, 5]
     """
-    arr = int_list[:] # Copy of original list
+    arr = int_list[:]  # Copy of original list
     n = len(arr)
 
     for i in range(n):
-        for j in range(0, n-i-1): # Traverse the list from 0 - n-
-            if arr[j] > arr[j+1]: # Swap element if grater than next element
-                arr[j], arr[j+1] = arr[j+1], arr[j]
+        for j in range(0, n - i - 1):  # Traverse the list from 0 - n-
+            if arr[j] > arr[j + 1]:  # Swap element if grater than next element
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
 
     print("bubble sort")
     return arr
+
 
 def quick(int_list):
     """
@@ -61,18 +63,18 @@ def quick(int_list):
         quick([3, 4, 5, 2, 1, 1, 5])
         quick sort[1, 1, 2, 3, 4, 5, 5]
     """
-    arr = int_list[:] # Copy of original list
+    arr = int_list[:]  # Copy of original list
 
     if len(arr) <= 1:
         return arr
-    pivot = arr[len(arr) // 2] # choose middle element as pivot
+    pivot = arr[len(arr) // 2]  # choose middle element as pivot
     left = [x for x in arr if x < pivot]
     middle = [x for x in arr if x == pivot]
     right = [x for x in arr if x > pivot]
 
-    
     print("quick sort")
     return quick(left) + middle + quick(right)
+
 
 def insertion(int_list):
     """
@@ -87,21 +89,21 @@ def insertion(int_list):
     Example:
 
         bubble([3, 4, 5, 2, 1, 1, 5])
-        insertion sort[1, 1, 2, 3, 4, 5, 5] 
+        insertion sort[1, 1, 2, 3, 4, 5, 5]
     """
-    arr = int_list[:] # Copy of original list
+    arr = int_list[:]  # Copy of original list
 
     for i in range(1, len(arr)):
-        key = arr[i] # Element to be positioned
+        key = arr[i]  # Element to be positioned
         j = i - 1
 
         # Shift elements of arr[0] - arr[i-1] that are greater than key
         while j >= 0 and key < arr[j]:
-            arr[j+1] = arr[j]
+            arr[j + 1] = arr[j]
             j -= 1
 
         # Place key at after the element just smaller than it
-        arr[j+1] = key
+        arr[j + 1] = key
 
     print("insertion sort")
-    return arr    
+    return arr
